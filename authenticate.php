39d3d333834715712ff495e13addd76e4921cb1a
Suresh <sureshkumar@winspire.co>
2021-09-30 13:44:27 +0530

    Mysql Config Changed

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
+$DATABASE_HOST = '127.0.0.1';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = 'sujaMysql';//'welcomeSuresh';
 $DATABASE_NAME = 'phplogin';
