ab321124d6658c3dd63ca3bb83b0876187245011
sujatce <sujatce@gmail.com>
2021-10-06 20:05:43 -0700

    checkin

--- home.php
+++ home.php
@@ -18,13 +18,13 @@ if (!isset($_SESSION['loggedin'])) {
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+				<h1>CovidLab</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Home Page</h2>
+			<h2>Welcome</h2>
 			<p>Hi <?=$_SESSION['name']?>!, you have logged in 5 times and Last Login date: 10/7/2021 9:00 PM</p>
 			<h2>Company Confidential Information</h2>
 			<p>CovidLab is committed to protecting the privacy of every person who visits the CovidLab Web site so that each person will feel free to gather information, make inquiries/comments, and/or perform bill payment functions on our site. As part of covidlab's effort to protect the privacy of your personal information while visiting the site, we created this web privacy statement to inform you of the privacy standards used to ensure the security and confidentiality of your information. </p>
