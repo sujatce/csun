9c461a2c424c477951521e763fbdda1326377856
sujatce <70561546+sujatce@users.noreply.github.com>
2021-10-07 18:21:41 -0700

    Delete CaptchaConfig.php

--- CaptchaConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// BotDetect PHP Captcha configuration options
-// ---------------------------------------------------------------------------
-$BotDetect->HelpLinkMode = HelpLinkMode::Image;
-
-?>
