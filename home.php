ce0e5958de3f0d0d041ecbc390c845cc11b99048
kcp99 <kinjal.patel.235@my.csun.edu>
2021-10-25 19:31:05 -0700

    to add download feature

--- home.php
+++ home.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['loggedin'])) {
 			<p>Hi <?=$_SESSION['name']?>!, you have logged in 5 times and Last Login date: 10/7/2021 9:00 PM</p>
 			<h2>Company Confidential Information</h2>
 			<p>CovidLab is committed to protecting the privacy of every person who visits the CovidLab Web site so that each person will feel free to gather information, make inquiries/comments, and/or perform bill payment functions on our site. As part of covidlab's effort to protect the privacy of your personal information while visiting the site, we created this web privacy statement to inform you of the privacy standards used to ensure the security and confidentiality of your information. </p>
-			<a href="">Download Company Confidential File</a>
+			<a href="" download>Download Company Confidential File </a>
 		</div>
 	</body>
 </html>
